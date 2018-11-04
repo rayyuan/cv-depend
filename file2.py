66d32b706c0cca609d6ad920f81a17e18c88eabd
Ray Yuan <ray.x.yuan@gmail.com>
2018-11-04 12:13:06 -0800

    lol

--- file2.py
+++ file2.py
@@ -1,10 +1,61 @@
 from file1.py import get_time()
-
+import file1
+import file3
 def file2func(arg1, arg2):
     get_time()
     pass
 
-
+def Important():
+    file1.reset()
+    file3.run2()
+    a=1
+    b=2
+    c=3
+    d=4
+    a=1
+    b=2
+    c=3
+    d=4
+    a=1
+    b=2
+    c=3
+    d=4
+    a=1
+    b=2
+    c=3
+    d=4
+    a=1
+    b=2
+    c=3
+    d=4
+    c=3
+    d=4
+    a=1
+    b=2
+    c=3
+    d=4
+    a=1
+    b=2
+    c=3
+    d=4
+    a=1
+    b=2
+    c=3
+    d=4
+    c=3
+    d=4
+    a=1
+    b=2
+    c=3
+    d=4
+    a=1
+    b=2
+    c=3
+    d=4
+    a=1
+    b=2
+    c=3
+    d=4
 def another_func():
     print()
     size(variable)
